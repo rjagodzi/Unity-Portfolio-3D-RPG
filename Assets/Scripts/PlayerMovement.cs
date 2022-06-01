8a09472d894f66d3c6b76b8c7f5585c3f132e830
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-06-01 16:04:19 +0200

    Now player's character has a click based movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                nav.destination = hit.point;
+            }
+        }
+    }
+}
