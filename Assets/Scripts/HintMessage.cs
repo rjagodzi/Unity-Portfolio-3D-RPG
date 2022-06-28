c3083492f16abb9ef992cf25920ff54605ae5fae
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-06-28 14:33:22 +0200

    Added inventory sounds

--- Assets/Scripts/HintMessage.cs
+++ Assets/Scripts/HintMessage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53f222dc477eec2fa95b3afef6413be3414c828e4647f52f1edfd2563318970c
-size 7135
+oid sha256:10214f046db92a25c6e0ae041eef36e2e71d5c6eca9cb1af0f8db1eaa158935d
+size 7373
--- Assets/Scripts/InventoryItems.cs
+++ Assets/Scripts/InventoryItems.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:683f31acd07d18439b6aa839448c8a9d5d8cf7cdb245550423ba94ca7f32d424
-size 5987
+oid sha256:a882cf1d4d4f2b2c4e3f0c421be8458955acd20e17df8a9a5e2162ec550f82e6
+size 6293
