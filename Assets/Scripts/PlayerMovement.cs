7d3614b63a02043e126cfc0a363f584e5724fdc1
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-06-29 14:10:44 +0200

    Now after clicking an enemy the player's character approaches it and stops at some distance

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c09876821edb87fe1bf8b55e0afeb3f416ac1eb29c09c90803d966485f0faa25
-size 3625
+oid sha256:9ff4ccf809a4784ad474bb7b0a8ab1d62c92cbf991114a4051489decdbb67066
+size 4124
