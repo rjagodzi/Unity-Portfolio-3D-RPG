0a64319e52e511136ee42094d1b8d03ab9b82157
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-06-30 15:14:59 +0200

    Created three single shot spells (Ultra Nova, Red Blast, Plasma Burst)

--- Assets/Scripts/MagicScripts/ParticleMover.cs
+++ Assets/Scripts/MagicScripts/ParticleMover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd26c9639b415cc37d46f64d08149a974eadbfacb83527e5a4489d207877e2d4
-size 1129
+oid sha256:5953028c4a3229ae24b82484698044a6e91e3dd7bfd7c37c4cf3021a3bf240a0
+size 1398
