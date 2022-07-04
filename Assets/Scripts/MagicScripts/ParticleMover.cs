6314c27589afeefed23f6a82721c02cf779d3fbd
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-07-04 17:47:53 +0200

    Modified some spell effects to follow the player

--- Assets/Scripts/MagicScripts/ParticleMover.cs
+++ Assets/Scripts/MagicScripts/ParticleMover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5953028c4a3229ae24b82484698044a6e91e3dd7bfd7c37c4cf3021a3bf240a0
-size 1398
+oid sha256:60731ef412ed21cd574de3f96c51dcc1a5394b281774252dc1d3bae836438bd7
+size 1673
