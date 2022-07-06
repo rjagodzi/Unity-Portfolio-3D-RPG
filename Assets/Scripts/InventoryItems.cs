a001f823f8ffd6661812a649be6a3a1db1b178f7
Wawrzyniec87 <wawrzyniec20@gmail.com>
2022-07-06 15:05:35 +0200

    Mana regenerates. Spells cannnot be used if mana equals 0

--- Assets/Scripts/InventoryItems.cs
+++ Assets/Scripts/InventoryItems.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d013760abc1cedd12d6a9836db773077dd8b7185b4800821f21ac4f975459fa
-size 7172
+oid sha256:39f2de5d085b53b086036510debfa35d74ca6ee093ce11752f73eab57984b014
+size 7288
--- Assets/Scripts/SaveScript.cs
+++ Assets/Scripts/SaveScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:812ea86ed6758a942daf98c118a2f7024bac16e98a9cc1621f78c6be1f6351e2
-size 854
+oid sha256:5ff9ad86e4f9ded8cb2223ceb6ba6797ecdcb6377f968ebe3f8000b6e1de6ee2
+size 937
